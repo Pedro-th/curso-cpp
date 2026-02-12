96f247614d8f73561cad22b07727e8d6bc917544
pedro thiago <pedropedrinhothiago410@gmail.com>
2026-02-12 12:40:30 -0300

    o escopo do jogo e a função pra ver se o chute de letra está na palavras

--- jogo_da_forca.cpp
+++ jogo_da_forca.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+bool letra_pertencente (char chute, string palavra) {
+    int i;
+    for (i=0; i < int(palavra.size()); i++) {
+        if (chute == palavra[i]) {
+            return true;
+        } 
+    }
+    return false;
+
+}
+
 int main () {
-    
+char chute;
+string palavra_secreta = "melancia";
+bool nao_acertou = true;
+bool nao_enforcou =  true;
+
+while (nao_acertou && nao_enforcou) {
+    cout << "qual seu chute: ";
+    cin >> chute;
+    if (letra_pertencente(chute,palavra_secreta)) {
+        cout << "seu chute esta na palavra secreta\n";
+    } else {
+        cout << "seu chute nao esta na palavra secreta\n";
+    }
+
+    }
+
 }
