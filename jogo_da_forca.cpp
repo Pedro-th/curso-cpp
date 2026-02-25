d5d668f8f6901e2fcdc91a5d8453197487312eff
pedro thiago <pedropedrinhothiago410@gmail.com>
2026-02-24 22:07:27 -0300

    inclusão de funções para parada de loop, criação de vetor para chutes errados e criação do cabeçalho

--- jogo_da_forca.cpp
+++ jogo_da_forca.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
+string palavra_secreta = "melancia";
+//dicionario em c++
+map<char,bool> chutou;
+//vetor dinamico
+vector<char> chutes_errados;
 
 bool letra_pertencente (char chute, string palavra) {
     int i;
@@ -11,22 +18,64 @@ bool letra_pertencente (char chute, string palavra) {
     return false;
 
 }
+//função que continua o jogo se o chute está na palavra secreta
+bool nao_acertou () {
+    for (char letra : palavra_secreta) {
+        if (!chutou[letra]) {
+            return true;
+        }
+    }
+    return false;
+}
+//fução que delimita o numero de tentativas pra 5, assim como no jogo da forca
+bool nao_enforcou () {
+    return chutes_errados.size() < 5;
+}
+
+
 
 int main () {
-char chute;
-string palavra_secreta = "melancia";
-bool nao_acertou = true;
-bool nao_enforcou =  true;
+    cout << "*******************************\n";
+    cout << "******* jogo da forca *********\n";
+    cout << "*******************************\n";
 
-while (nao_acertou && nao_enforcou) {
-    cout << "qual seu chute: ";
+char chute;
+
+while (nao_acertou() && nao_enforcou()) {
+    cout << "chutes errados: ";
+    for (char letra : chutes_errados) {
+        cout << letra << " ";
+    } 
+    cout << endl;
+    //esse for intera a letra atraves da palavra secreta
+    for (char letra : palavra_secreta) {
+        if (chutou[letra]) {
+            cout << letra << " ";
+        } else {
+            cout << "_ ";
+        }
+    }
+    
+    cout <<"\n qual seu chute: ";
     cin >> chute;
+    
+    chutou[chute] = true;
+
     if (letra_pertencente(chute,palavra_secreta)) {
         cout << "seu chute esta na palavra secreta\n";
     } else {
         cout << "seu chute nao esta na palavra secreta\n";
+        chutes_errados.push_back(chute);
+
     }
 
     }
+    cout << "fim de jogo" << endl;
+    if (nao_acertou()) {
+        cout << "vc perdeu, tente novamente";
+    } else {
+        cout << "parabens, vc ganhou";
+    }
+
 
 }
